522fffab301baf289541097b5126c9566ee27a85
Arweenn <arwendumont25@outlook.fr>
2023-11-23 08:08:28 -0800

    function tester

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *street = "Rue des Marchands";
+	char *city = "Toulouse";
+	char state = 'F';
+
+	_printf("%d, %s, %s %c %i\n", 37, street, city, state, 31000);
+
+	return (0);
+}
